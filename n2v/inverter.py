eabbd7da2afc8cfcb6b01cd517c8abd9a94e31d9
Victor H. Chavez <gonza445@purdue.edu>
2020-12-26 15:51:44 -0500

    Add JK objects and Initial guess for inversion

--- n2v/inverter.py
+++ n2v/inverter.py
@@ -29,10 +29,21 @@ def Inverter()
             raise ValueError("Reference is set as Unrestricted but target density's dimension == 1")
         if self.ref == "RKS" or self.ref == "RHF" and len(nt) == 2:
             raise ValueError("Reference is set as Restricted but target density's dimension == 2")
+        if len(nt == 1):
+            nt.append(nt[0])
 
         #Generate Basis set
         self.build_basis()
+        #Generate matrices from Mints
+        self.generate_mint_matrices()
+        self.generate_core_matrices()
+        self.generate_jk()
 
+        #Plotting Grid
+        self.grid = Grider()
+
+
+    #-------------  Basics
 
     def build_basis(self):
         """
@@ -42,41 +53,104 @@ def Inverter()
         basis = psi4.core.BasisSet.build( mol, key='BASIS', target=self)
         self.basis = basis
         self.nbf   = self.basis.nbf()
-        self.naux  = self.basis.nbf()
 
         if self.aux_str is not "same":
             aux_basis = psi4.core.BasisSet.build( self.mol, key='Basis', target=self.aux_str)
             self.aux = aux_basis
             self.naux = aux_basis.nbf()
+        else:
+            self.aux  = self.basis
+            self.naux = self.nbf
+
+    def generate_mints_matrices(self):
+        """
+        Generates matrices that are methods of a mints object
+        """
+
+        mints = psi4.core.MintsHelper( self.basis )
+
+        #Overlap Matrices
+        self.S2 = mints.ao_overlap().np
+        A = mints.ao_overlap()
+        A.power( -0.5, 1e-16 )
+        self.A = A
+        self.S3 = np.squeeze(mints.ao_3coverlap(self.basis,self.basis,self.aux))
+        self.jk = None 
+
+        #Core Matrices
+        self.T = self.mints.ao_kinetic().np.copy()
+        self.V = self.mints.ao_potential().np.copy()
+
+        self.mints = mints
+
+    def generate_jk(self, gen_K=True, memory=2.50e9):
+        """
+        Creates jk object for generation of Coulomb and Exchange matrices
+        2.5e9 B -> 2.5 GB
+        """
+        jk = psi4.core.JK.build(self.basis)
+        jk.set_memory(int(memory)) 
+        jk.set_do_K(gen_K)
+        jk.initialize()
+
+    def form_jk(self, coca, cocb):
+        """
+        Generates Coulomb and Exchange matrices from occupied orbitals
+        """
+
+        self.jk.C_left_add(C_occ_a)
+        self.jk.C_left_add(C_occ_b)
+        self.jk.compute()
+        self.jk.C_clear()
+
+        J = [self.jk.J()[0], self.jk.J()[1]]
+        K = [self.jk.K()[0], self.jk.K()[1]]
+
+        return J, K
+
+
+
+
+    #Inversion
+
+    def invert(self, method, guess):
+
+        self.initial_guess(method)
+
+
+    def initial_guess(self, method):
+
+        self.guess_a = 
+        self.guess_b =
+
+        if "fermi_amaldi" in method:
+
+
+        if "svwn" in method or "pbe" in method:
+            if self.debug == True:
+                print(f"Adding XC potential {method} to initial guess")
+
+            _, wfn_guess = psi4.energy( method+/+self.basis_str, molecule=self.mol , return_wfn = True)
+            na_target = self.nt[0]
+            nb_target = self.nt[1]
+            #Get density-drivenless vxc
+            wfn_guess.V_potential().set_D( [na_target, nb_target] )
+            va_target = psi4.core.Matrix( self.nbf, self.nbf )
+            vb_target = psi4.core.Matrix( self.nbf, self.nbf )
+            wfn_guess.V_potential().compute_V([va_target, vb_target])
+
+            self.guess_a += va_target
+            self.guess_b += vb_target
 
 
 
 
 
 
-def canvas(with_attribution=True):
-    """
-    Placeholder function to show example docstring (NumPy format)
-
-    Replace this function and doc string for your own project
-
-    Parameters
-    ----------
-    with_attribution : bool, Optional, default: True
-        Set whether or not to display who the quote is from
-
-    Returns
-    -------
-    quote : str
-        Compiled string including quote and optional attribution
-    """
-
-    quote = "The code is but a canvas to our imagination."
-    if with_attribution:
-        quote += "\n\t- Adapted from Henry David Thoreau"
-    return quote
 
 
-if __name__ == "__main__":
-    # Do something if this file is invoked on its own
-    print(canvas())
+
+
+
+
+
