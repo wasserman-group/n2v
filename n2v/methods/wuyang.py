88a0b061c976a68ae5dd4a3d928c211a7cd85a3e
Victor H. Chavez <gonza445@purdue.edu>
2020-12-28 13:37:10 -0500

    Add initial guess potential to lagrangian

--- n2v/methods/wuyang.py
+++ n2v/methods/wuyang.py
@@ -8,111 +8,130 @@ import numpy as np
 from opt_einsum import contract
 from scipy.optimize import minimize
 
-def wuyang(self, opt_method):
-
-    if opt_method == 'bfgs':
-        opt_results = minimize( fun = lagrangian,
-                                x0  = self.v0, 
-                                jac = gradient,
-                                method = opt_method,
-                                # tol    = 1e-6,
-                                options = {"maxiter" : 20,
-                                            "disp"    : False,}
-                                )
-
-    else:
-        opt_results = minimize( fun = lagrangian,
-                                x0  = self.v0, 
-                                jac = gradient,
-                                hess = hessian,
-                                method = opt_method,
-                                # tol    = 1e-6,
-                                options = {"maxiter" : 20,
-                                            "disp"    : False, }
-                                )
-
-    if opt_results.success == False:
-        raise ValueError("Optimization was unsucessful, try a different intitial guess")
-
-    # self.finalize_energy()
-
-    # if debug=True:
-    #     self.density_accuracy()
-
-    self.v = opt_results()
-
-def diagonalize_with_guess(self, v):
+class WuYang():
     """
-    Diagonalize Fock matrix with additional external potential
-    """
-    vks_a = contract("ijk,k->ij", self.S3, v[:self.nbf]) + self.guess_a
-    fock_a = self.V + self.T + vks_a 
-    self.Ca, self.Coca, self.Da, self.eigvecs_a = self.diagonalize( fock_a, self.nalpha )
-
-    if self.ref == "UHF" or self.ref == "UKS":
-        vks_b = contract("ijk,k->ij", self.S3, v[self.nbf:]) + self.guess_b
-        fock_b = self.V + self.T + vks_b        
-        self.Cb, self.Cocb, self.Db, self.eigvecs_b = self.diagonalize( fock_b, self.nbeta )
-    else:
-        self.Cb, self.Coca, self.Db, self.eigvecs_b = self.Ca.copy(), self.Coca.copy(), self.Da.copy(), self.eigvecs_b.copy()
-
-def lagrangian(self, v):
-    """
-    Lagrangian to be minimized wrt external potential
+    Performs Optimization as in: 10.1063/1.1535422 - Qin Wu + Weitao Yang
     """
 
-    diagonalize_with_guess(self, v)
+    def wuyang(self, opt_method):
+        """
+        Calls scipy minimizer to minimize lagrangian. 
+        """
 
-    kinetic    = - contract('ij,ji', self.part.T, (self.Da + self.Db))
-    potential  =   contract('ij,ji', self.part.V, (self.nt[0] + self.nt[1]) - (self.Da + self.Db))
-    optimizing =   contract('i,i'  , v, self.grad)
+        if opt_method == 'bfgs':
+            opt_results = minimize( fun = self.lagrangian,
+                                    x0  = self.v0, 
+                                    jac = self.gradient,
+                                    method = opt_method,
+                                    tol    = 1e-7,
+                                    options = {"maxiter" : 50,
+                                                "disp"    : False,}
+                                    )
 
-    if self.debug == True:
-        print(f"Kinetic: {kinetic} | Potential {potential} | From Optimization {optimizing}")
+        else:
+            opt_results = minimize( fun = self.lagrangian,
+                                    x0  = self.v0, 
+                                    jac = self.gradient,
+                                    hess = self.hessian,
+                                    method = opt_method,
+                                    tol    = 1e-7,
+                                    options = {"maxiter" : 50,
+                                                "disp"    : False, }
+                                    )
 
-    L = kinetic + potential + optimizing
+        if opt_results.success == False:
+            raise ValueError("Optimization was unsucessful, try a different intitial guess")
+        else:
+            print("Optimization Successful")
 
-    reg = 0.0
-    if self.reg > 0:
-        pass
+        self.finalize_energy()
+        self.generate_grid()
 
-    return L - reg
+        # if debug=True:
+        #     self.density_accuracy()
 
-def gradient(self, v):
-    """
-    Calculates gradient wrt target density
-    """
-    diagonalize_with_guess(self, v)
+        self.v = opt_results.x
 
-    self.grad_a = contract('ij,ijt->t', (self.nt[0] - self.Da), self.part.S3)
-    self.grad_b = contract('ij,ijt->t', (self.nt[1] - self.Db), self.part.S3) 
-    self.grad   = np.concatenate(( self.grad_a, self.grad_b ))
+    def diagonalize_with_guess(self, v):
+        """
+        Diagonalize Fock matrix with additional external potential
+        """
+        vks_a = contract("ijk,k->ij", self.S3, v[:self.nbf]) + self.guess_a
+        fock_a = self.V + self.T + vks_a 
+        self.Ca, self.Coca, self.Da, self.eigvecs_a = self.diagonalize( fock_a, self.nalpha )
 
-    return self.grad
+        if self.ref == "UHF" or self.ref == "UKS":
+            vks_b = contract("ijk,k->ij", self.S3, v[self.nbf:]) + self.guess_b
+            fock_b = self.V + self.T + vks_b        
+            self.Cb, self.Cocb, self.Db, self.eigvecs_b = self.diagonalize( fock_b, self.nbeta )
+        else:
+            self.Cb, self.Coca, self.Db, self.eigvecs_b = self.Ca.copy(), self.Coca.copy(), self.Da.copy(), self.eigvecs_a.copy()
 
-def hessian(self, v):
+    def lagrangian(self, v):
+        """
+        Lagrangian to be minimized wrt external potential
+        Equation (5) of main reference
+        """
 
-    diagonalize_with_guess(self, v)
+        self.diagonalize_with_guess(v)
+        self.grad_a = contract('ij,ijt->t', (self.Da - self.nt[0]), self.S3)
+        self.grad_b = contract('ij,ijt->t', (self.Db - self.nt[1]), self.S3) 
 
-    na, nb = self.nalpha, self.nbeta
+        kinetic     =   contract('ij,ji', self.T, (self.Da + self.Db))
+        potential   =   contract('ij,ji', self.V + self.guess_a, self.Da - self.nt[0])
+        potential  +=   contract('ij,ji', self.V + self.guess_b, self.Db - self.nt[1])
+        optimizing  =   contract('i,i'  , v[:self.naux], self.grad_a)
+        optimizing +=   contract('i,i'  , v[self.naux:], self.grad_b)
 
-    eigs_diff_a = self.eigvecs_a[:na, None] - self.eigvecs_a[None, na:]
-    C3a = contract('mi,va,mvt->iat', self.Ca[:,:na], self.Ca[:,nb:], self.S3)
-    Ha = 2 * contract('iau,iat,ia->ut', C3a, C3a, eigs_diff_a**-1)
+        if self.debug == True:
+            print(f"Kinetic: {kinetic:6.4f} | Potential {np.abs(potential):6.4e} | From Optimization {np.abs(optimizing):6.4e}")
+        L = kinetic + potential + optimizing
 
-    if self.ref == "UHF" or self.ref == "UKS":
-        eigs_diff_b = self.eigvecs_b[:nb, None] - self.eigvecs_b[None, nb:]
-        C3b = contract('mi,va,mvt->iat', self.Cb[:,:nb], self.Cb[:,nb:], self.S3)
-        Hb = 2 * contract('iau,iat,ia->ut', C3b, C3b, eigs_diff_b**-1)
-    else:
-        Hb = Ha.copy()
+        reg = 0.0
+        if self.reg > 0:
+            pass
 
-    Hs = np.block( 
-                    [[Ha, np.zeros((self.naux, self.naux))],
-                        [np.zeros((self.naux, self.naux)), Hb]] 
-                    )
+        return - L - reg
 
-    if self.reg > 0.0:
-        pass
+    def gradient(self, v):
+        """
+        Calculates gradient wrt target density
+        Equation (11) of main reference
+        """
+        self.diagonalize_with_guess(v)
+        self.grad_a = contract('ij,ijt->t', (self.Da - self.nt[0]), self.S3)
+        self.grad_b = contract('ij,ijt->t', (self.Db - self.nt[1]), self.S3) 
+        self.grad   = np.concatenate(( self.grad_a, self.grad_b ))
 
-    return Hs
+        return -self.grad
+
+    def hessian(self, v):
+        """
+        Calculates gradient wrt target density
+        Equation (13) of main reference
+        """
+
+        self.diagonalize_with_guess(v)
+
+        na, nb = self.nalpha, self.nbeta
+
+        eigs_diff_a = self.eigvecs_a[:na, None] - self.eigvecs_a[None, na:]
+        C3a = contract('mi,va,mvt->iat', self.Ca[:,:na], self.Ca[:,nb:], self.S3)
+        Ha = 2 * contract('iau,iat,ia->ut', C3a, C3a, eigs_diff_a**-1)
+
+        if self.ref == "UHF" or self.ref == "UKS":
+            eigs_diff_b = self.eigvecs_b[:nb, None] - self.eigvecs_b[None, nb:]
+            C3b = contract('mi,va,mvt->iat', self.Cb[:,:nb], self.Cb[:,nb:], self.S3)
+            Hb = 2 * contract('iau,iat,ia->ut', C3b, C3b, eigs_diff_b**-1)
+        else:
+            Hb = Ha.copy()
+
+        Hs = np.block( 
+                        [[Ha, np.zeros((self.naux, self.naux))],
+                            [np.zeros((self.naux, self.naux)), Hb]] 
+                        )
+
+        if self.reg > 0.0:
+            pass
+
+        return - Hs
