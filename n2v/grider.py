91b2f424e31ae14f568fe2fe0b10692dd4af087e
Victor H. Chavez <gonza445@purdue.edu>
2021-01-01 14:25:26 -0500

    ESP on grid is no longer done by default

--- n2v/grider.py
+++ n2v/grider.py
@@ -10,7 +10,14 @@ import psi4
 psi4.core.be_quiet()
 from pylibxc import LibXCFunctional as Functional
 
+from time import time
+
 from .cubeprop import Cubeprop
+
+try:
+    from rich import print
+except:
+    pass
 
 import warnings
 import matplotlib.pyplot as plt
@@ -23,14 +30,16 @@ class data_bucket:
 class Grider(Cubeprop):
 
     def build_rectangular_grid(self, 
-                               L = [3.1, 3.1, 3.1], 
-                               D = [0.09, 0.09, 0.09]):
+                               L = [3.0, 3.0, 3.0], 
+                               D = [0.2, 0.2, 0.2], 
+                               elements = ["density", "ext", "xc"],
+                               one_dim = False):
 
         psi4.set_options({"CUBIC_BLOCK_MAX_POINTS" : 1000000})
                     
         O, N = self.build_grid(L, D)
         block, points, nxyz, npoints, grid =  self.populate_grid(O, N, D)
-        # return block, points, nxyz, npoints, [x_plot, y_plot, z_plot]
+
 
         # lpos = np.array(block.functions_local_to_global())
         # phi = points_func.basis_values()["PHI"])[:npoints, :lpos.shape[0]]
@@ -40,87 +49,91 @@ class Grider(Cubeprop):
         z = grid[2]
 
         # GENERATE DENSITY
-        density = np.zeros(int(npoints))
-        points.set_pointers(psi4.core.Matrix.from_array(self.Da))
-        rho = points.point_values()["RHO_A"]
-        offset = 0
-        for i in range(len(block)):
-            points.compute_points(block[i])
-            n_points = block[i].npoints()
-            offset += n_points
-            density[offset-n_points:offset] = 0.5 * rho.np[:n_points]
-        cube_density = np.reshape(density, (int(N[0]), int(N[1]), int(N[2])))
+        if "density" in elements:
+            density = np.zeros(int(npoints))
+            points.set_pointers(psi4.core.Matrix.from_array(self.Da))
+            rho = points.point_values()["RHO_A"]
+            offset = 0
+            for i in range(len(block)):
+                points.compute_points(block[i])
+                n_points = block[i].npoints()
+                offset += n_points
+                density[offset-n_points:offset] = 0.5 * rho.np[:n_points]
 
         self.grid = data_bucket
-        # self.grid.x, self.grid.y, self.grid.z = grid[0], grid[1], grid[2]
         self.grid.xyz = np.array((grid[0],grid[1],grid[2])).T    
-        self.grid.cube_density = cube_density
-        self.grid.scatter_density = density 
-
+        self.grid.density = density 
 
         #GENERATE EXTERNAL
-        mol_dict = self.mol.to_schema(dtype='psi4')
-        natoms = len(mol_dict["elem"])
-        indx = [i for i in range(natoms) if self.mol.charge(i) != 0.0]
-        natoms = len(indx)
-        #Atomic numbers and Atomic positions
-        zs = [mol_dict["elez"][i] for i in indx]
-        rs = [self.mol.geometry().np[i] for i in indx]
+        if "external" in elements:
+            mol_dict = self.mol.to_schema(dtype='psi4')
+            natoms = len(mol_dict["elem"])
+            indx = [i for i in range(natoms) if self.mol.charge(i) != 0.0]
+            natoms = len(indx)
+            #Atomic numbers and Atomic positions
+            zs = [mol_dict["elez"][i] for i in indx]
+            rs = [self.mol.geometry().np[i] for i in indx]
 
-        vext = np.zeros(npoints)
-        for atom in range(natoms):
-            r =  np.sqrt( (x-rs[atom][0])**2 + (y-rs[atom][1])**2+ (z-rs[atom][2])**2)
-            vext += -1.0 * zs[atom] / r
-            # vext[r < 1e-15] = 0                           
-        self.grid.vext = vext
+            vext = np.zeros(npoints)
+            for atom in range(natoms):
+                r =  np.sqrt( (x-rs[atom][0])**2 + (y-rs[atom][1])**2+ (z-rs[atom][2])**2)
+                vext += -1.0 * zs[atom] / r
+                vext[r < 1e-15] = 0                           
+            self.grid.vext = vext
 
-        #ESP
-        grid_block = np.array((x,y,z)).T    
-        esp_wfn = psi4.core.ESPPropCalc(self.wfn)
-        grid_block = psi4.core.Matrix.from_array(grid_block)
-        esp = esp_wfn.compute_esp_over_grid_in_memory(grid_block).np
-        self.grid.esp = esp
+        #HARTREE
+        if "hartree" in elements:
+            print("Computing ESP, Hang on tight")
+            esp_wfn = psi4.core.ESPPropCalc(self.wfn)
+            grid_block = psi4.core.Matrix.from_array(self.grid.xyz)
+            esp = esp_wfn.compute_esp_over_grid_in_memory(grid_block).np
+            self.grid.esp = esp
 
-        #Hartree
-        hartree = - 1.0 * (vext + esp)
-        self.grid.hartree = hartree
+            t5 = time() - t4
+            print(f"Generating ESP: {t5}")
+
+            #Hartree
+            hartree = - 1.0 * (vext + esp)
+            self.grid.hartree = hartree
+
+            t6 = time() - t5
+            print(f"Generate Hartree: {t4}")
 
         #XC
-        warnings.warn("Only LDA fucntionals are supported to be represented on the grid")
-        ingredients = {}
-        ingredients["rho"] = density
-        functional = Functional(1, 1)
-        xc_dictionary = functional.compute(ingredients)
-        vxc = xc_dictionary['vrho']  
+        if "xc" in elements:
+            warnings.warn("Only LDA fucntionals are supported to be represented on the grid")
+            ingredients = {}
+            ingredients["rho"] = density
+            functional = Functional(1, 1)
+            xc_dictionary = functional.compute(ingredients)
+            vxc = xc_dictionary['vrho']  
 
-        self.grid.vxc = vxc
+            self.grid.vxc = vxc
 
-        #From Optz
+        #GENERATE ONE DIMENSION
+        if one_dim is True:
+            indx  = []
+            for i in range(len(grid[0])):
+                if np.abs(grid[0][i]) < 1e-8:
+                    if np.abs(grid[1][i]) < 1e-8:
+                        indx.append( i )
+            if len(indx) == 0:
+                warnings.warn("Warning. Cubic grid is not covering the z axis")
+            indx = np.array(indx)
 
-        #Generate ONE DIMENSIONAL
-    
+            self.grid.z         = grid[2][indx]
+            self.grid.z_density = density[indx]
+            self.grid.z_esp     = esp[indx]
+            self.grid.z_hartree = hartree[indx]
+            self.grid.z_vext     = vext[indx]
+            self.grid.z_vxc      = vxc[indx]
 
-        indx  = []
-        for i in range(len(grid[0])):
-            if np.abs(grid[0][i]) < 1e-8:
-                if np.abs(grid[1][i]) < 1e-8:
-                    indx.append( i )
-        if len(indx) == 0:
-            warnings.warn("Warning. Cubic grid is not covering the z axis")
-        indx = np.array(indx)
-
-        self.grid.z         = grid[2][indx]
-        self.grid.z_density = density[indx]
-        self.grid.z_esp     = esp[indx]
-        self.grid.z_hartree = hartree[indx]
-        self.grid.z_vext     = vext[indx]
-        self.grid.z_vxc      = vxc[indx]
-
+        #GENERANTE 2 DIMENSIONS
+        cube_density = np.reshape(density, (int(N[0]), int(N[1]), int(N[2])))
+        self.grid.cube_density = cube_density
 
         psi4.set_options({"CUBIC_BLOCK_MAX_POINTS" : 1000})
-
-        block, points = 0, 0
-
+        block, points = None, None
         return
         
     # def generate_hartree(self):
