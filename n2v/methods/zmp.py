be0ba22b33cc7a034eccb7393f6be1cb510803b0
V.H. Chavez <gonza445@purdue.edu>
2021-04-11 11:18:56 -0400

    Includes option to print scf steps, save eigenvalues to inverter

--- n2v/methods/zmp.py
+++ n2v/methods/zmp.py
@@ -18,6 +18,7 @@ class ZMP():
             lambda_list=[70],
             zmp_functional='hartree',
             zmp_mixing = 1,
+            print_scf = False, 
             ):
 
         """
@@ -57,12 +58,13 @@ class ZMP():
         self.mixing = zmp_mixing
 
         print("\nRunning ZMP:")
-        self.zmp_scf(lambda_list, zmp_functional, opt_max_iter, D_conv=opt_tol)
+        self.zmp_scf(lambda_list, zmp_functional, opt_max_iter, print_scf, D_conv=opt_tol)
 
     def zmp_scf(self, 
             lambda_list,
             zmp_functional,
             maxiter, 
+            print_scf,
             D_conv):
         """
         Performs scf cycle
@@ -243,10 +245,11 @@ class ZMP():
                 derror = np.max(np.abs(ddm))
 
                 #Uncomment to debug internal SCF
-                # if len(lambda_list==1) and np.mod(SCF_ITER,5) == 0.0:
-                #    print(f"Iteration: {SCF_ITER:3d} | Self Convergence Error: {derror:10.5e} | DIIS Error: {diis_error:10.5e}")
+                if print_scf is True:
+                    if np.mod(SCF_ITER,5) == 0.0:
+                        print(f"Iteration: {SCF_ITER:3d} | Self Convergence Error: {derror:10.5e} | DIIS Error: {diis_error:10.5e}")
                 
-                if abs(derror) < D_conv and abs(diis_error) < D_conv:
+                if abs(derror) < D_conv: #and abs(diis_error) < D_conv:
                     break
                 if SCF_ITER == maxiter - 1:
                     raise ValueError("Maximum Number of SCF cycles reached. Try different settings.")
@@ -282,6 +285,7 @@ class ZMP():
         self.Da = Da
         self.Ca = Ca
         self.Coca = Cocca
+        self.eigs_a = eigs_a
 
         if self.ref == 2:
             self.proto_density_b -= lam_i * (Db - self.nt[1]) * self.mixing
@@ -290,12 +294,14 @@ class ZMP():
             self.Db = Db
             self.Cb = Cb
             self.Cocb = Coccb
+            self.eigs_b = eigs_a
 
         else:
             self.proto_density_b = self.proto_density_a.copy()
             self.Db = self.Da.copy()
             self.Cb = self.Ca.copy()
             self.Cocb = self.Coca.copy()
+            self.eigs_b = self.eigs_a.copy()
 
 
     def generate_s_functional(self, lam, zmp_functional, Cocca, Coccb, Da, Db):
