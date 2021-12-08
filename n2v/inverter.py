c62741374afc5c6349f7b5e8186488897d96a828
V.H. Chavez <gonza445@purdue.edu>
2021-12-08 14:48:40 -0500

    Add pyscf engine

--- n2v/inverter.py
+++ n2v/inverter.py
@@ -7,6 +7,8 @@ import numpy as np
 from opt_einsum import contract
 
 from .engines import Psi4Engine
+from .engines import PySCFEngine
+
 from .methods.zmp import ZMP
 from .methods.wuyang import WuYang
 from .methods.pdeco import PDECO
@@ -28,8 +30,10 @@ class Inverter(ZMP, WuYang, PDECO, OC):
         self.eng_str = engine
         if engine == 'psi4':
             self.eng = Psi4Engine()
-        # elif:
-        #     raise ValueError("Engine name is incorrect. Only psi4 engine is currently available")
+        elif engine == 'pyscf':
+            self.eng = PySCFEngine()
+        else:
+            raise ValueError("Engine name is incorrect. The availiable engines are: {psi4, pyscf}")
             
     def __repr__( self ):
         return "n2v.Inverter"
@@ -38,11 +42,11 @@ class Inverter(ZMP, WuYang, PDECO, OC):
         # Communicate TO engine
 
         self.eng.set_system(molecule, basis, ref, pbs)
-        self.mol_str = self.eng.mol_str
+        # self.mol_str = self.eng.mol_str
         self.ref = ref
 
-        # self.nalpha = None
-        # self.nbeta = None
+        self.nalpha = self.eng.nalpha
+        self.nbeta = self.eng.nbeta
 
         # Initialize ecompasses everything the engine builds with basis set 
         self.eng.initialize()
@@ -60,15 +64,15 @@ class Inverter(ZMP, WuYang, PDECO, OC):
         self.V  = self.eng.get_V()
         self.A  = self.eng.get_A()
         self.S2 = self.eng.get_S()
+        self.S3      = self.eng.get_S3()
 
-        if self.eng.basis_str != 'same':  
-            self.S3      = self.eng.get_S3()
+        if self.eng.pbs_str != 'same':  
             self.T_pbas  = self.eng.get_Tpbas()
 
         self.S4 = None
 
-    def compute_hartree( self, Cooc_a, Cooc_b ):
-        return self.eng.compute_hartree(Cooc_a, Cooc_b)
+    def compute_hartree( self, *args ):
+        return self.eng.compute_hartree(*args)
 
     def diagonalize( self, matrix, ndocc ):
         """
@@ -110,12 +114,11 @@ class Inverter(ZMP, WuYang, PDECO, OC):
         N       = self.nalpha + self.nbeta
 
         if guide_components == 'none':
-            warn("Convergence will likely not be achieved")
+            warn("No guide potential was provided. Convergence may not be achieved")
         elif guide_components == 'hartree':
             self.va += self.J0[0]
             self.vb += self.J0[1]
         elif guide_components == 'fermi_amaldi':
-            "Im calculating Fermi Amaldi Component"
             print("Computing Fermi Amaldi")
             v_fa = (1-1/N) * (self.J0[0] + self.J0[1])
             self.va += v_fa
@@ -144,5 +147,4 @@ class Inverter(ZMP, WuYang, PDECO, OC):
             return self.pdeco(opt_max_iter, **keywords)
         else:
             raise ValueError(f"Inversion method not available. Methods available: {['wuyang', 'zmp', 'mrks', 'oc', 'pdeco']}")
-
 
