d56cdd274ebee3c22ef355785155bac32a860749
V.H. Chavez <gonza445@purdue.edu>
2022-03-01 20:21:31 -0500

    Update inits for optional packages

--- n2v/grid/__init__.py
+++ n2v/grid/__init__.py
@@ -1,2 +1,16 @@
-from .psi4grider import Psi4Grider
-from .pyscfgrider import PySCFGrider
+try:
+    import pyscf
+    has_pyscf = True
+except ImportError:
+    has_pyscf = False
+
+try:
+    import psi4
+    has_psi4 = True
+except ImportError:
+    has_psi4 = False
+
+if has_pyscf:
+    from .pyscfgrider import PySCFGrider
+if has_psi4:
+    from .psi4grider import Psi4Grider
